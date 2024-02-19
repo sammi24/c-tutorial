dbe202cd4d6841a68ba9a805320348ca939d3d11
sammi ghazzzawi <swghazzawi@gmail.com>
2024-02-19 14:49:50 -0700

    commit

--- file.c
+++ file.c
@@ -2,6 +2,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/stat.h>
 
 struct database_header_t
 {
@@ -12,6 +13,10 @@ struct database_header_t
 
 int main(int argc, char *argv[])
 {
+
+  struct database_header_t head = {0};
+  struct stat dbStat = {0};
+
   if (argc != 2)
   {
     printf("Usage: %s <filename>\n", argv[0]);
@@ -25,8 +30,32 @@ int main(int argc, char *argv[])
     return -1;
   }
 
-  char *mydata = "hello there file!\n";
-  write(fd, mydata, strlen(mydata));
+  if (read(fd, &head, sizeof(head)) != sizeof(head))
+  {
+    perror("read");
+    close(fd);
+    return -1;
+  }
+
+  printf("DB Version: %u\n", head.version);
+  printf("DB Number of Employees: %u\n", head.employees);
+  printf("DB File Length: %u\n", head.filesize);
+
+  if (fstat(fd, &dbStat) < 0)
+  {
+    perror("fstat");
+    close(fd);
+    return -1;
+  }
+
+  printf("DB File Length, reported by stat: %lu\n", dbStat.st_size);
+
+  if (dbStat.st_size != head.filesize)
+  {
+    printf("HACKER\n");
+    close(fd);
+    return -2;
+  }
 
   close(fd);
   return 0;
--- tutorial.c
+++ tutorial.c
@@ -2,46 +2,8 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-#define MAX_EMPLOYEES 1000
-
-struct employee_t
+int main(int argc, char *argv[])
 {
-  int id;
-  int income;
-  bool staff;
-};
-
-int initialize_ee(struct employee_t *e)
-{
-  static int numEmployees = 0;
-  numEmployees++;
-
-  e->id = numEmployees;
-  e->income = 0;
-  e->staff = false;
-
-  return numEmployees;
-}
-
-int main()
-{
-  // this value is from database header
-  int n = 4;
-
-  struct employee_t *employees = malloc(sizeof(struct employee_t) * n);
-  if (employees == NULL)
-  {
-    printf("The allocator failed\n");
-    return -1;
-  }
-
-  for (int i = 0; i < n; i++)
-  {
-    int id = initialize_ee(&employees[i]);
-    printf("New Employee, ID is %d\n", id);
-  }
-  free(employees);
-  employees = NULL;
 
   return 0;
 }
